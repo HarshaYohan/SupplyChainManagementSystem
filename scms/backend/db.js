2b4f66f38386063eb093ffec739071709891d2ff
Dulakshi <dulakshiabeynayake@gmail.com>
2024-10-22 10:52:49 +0530

    Updated Driver/DriverHomePage/ MyEffort

--- scms/backend/db.js
+++ scms/backend/db.js
@@ -2,11 +2,11 @@ import mysql from "mysql2";
 
 let db;
 db = mysql.createConnection({
-  // host: "localhost",
-  // user: "root",
-  // password: "#2024mySQLdatabase",
-  // database: "supplychainmanagementsystem",
-  // port: 3307,
+  host: "localhost",
+  user: "root",
+  password: "Dulakshi@2002",
+  database: "supplychainsystem",
+  port: 3306,
   host: process.env.DATABASE_HOST,
   user:  process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
